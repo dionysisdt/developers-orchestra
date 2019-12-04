980cd0818413c494a1ec5ffbdbefca33e7bb7726
Dionysios Theodoropoulos <dionysios.theodoropoulos@gmail.com>
2019-12-04 21:46:06 +0200

    Created the pom.xml

---
+++ src/main/java/com/projectfuture/team0/developersorchestra/DevelopersOrchestraApplication.java
@@ -0,0 +1,13 @@
+package com.projectfuture.team0.developersorchestra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopersOrchestraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopersOrchestraApplication.class, args);
+	}
+
+}
