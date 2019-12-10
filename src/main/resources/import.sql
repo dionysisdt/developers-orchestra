2bf73733a17a79664f19ff98a8884990aaad387d
kostasbls <kostasmprellas@yahoo.gr>
2019-12-10 18:09:48 +0200

    Added more queries to import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,13 +1,35 @@
 INSERT INTO OWNER (tax_id, fname, lname, address, phone_number, email, `password`, property_type, user_privileges) VALUES('098765432', 'Dimitris', 'Maleas', 'Sokaki1', '6574847586','dimitris@gmail.com', '123457', 'APARTMENT_BUILDING', 'ADMIN');
 INSERT INTO OWNER (tax_id, fname, lname, address, phone_number, email, `password`, property_type, user_privileges) VALUES('123456789', 'Kwstas', 'Brellas', 'Sok', '4758693048','kwstas@gmail.com', '123456', 'VILLA', 'ADMIN');
 INSERT INTO OWNER (tax_id, fname, lname, address, phone_number, email, `password`, property_type, user_privileges) VALUES('584739201', 'Dimitra', 'Tsolakou', 'Sokaki2', '2049506539','dimitra@gmail.com', '123458', 'VILLA', 'ADMIN');
-INSERT INTO OWNER (tax_id, fname, lname, address, phone_number, email, `password`, property_type, user_privileges) VALUES('506937485', 'Dionisis', 'Theo', 'Sokaki3', '6384956301','nionios@gmail.com', '123459', 'COTTAGE', 'USER');
+INSERT INTO OWNER (tax_id, fname, lname, address, phone_number, email, `password`, property_type, user_privileges) VALUES('506937485', 'Dionisis', 'Theo', 'Sokaki3', '6384956301','nionios@gmail.com', '123459', 'COTTAGE', 'ADMIN');
 INSERT INTO OWNER (tax_id, fname, lname, address, phone_number, email, `password`, property_type, user_privileges) VALUES('557748395', 'Xristos', 'Peristeris', 'Sokaki', '4059638265','xristos@gmail.com', '123450', 'DETACHED_HOUSE', 'USER');
+INSERT INTO OWNER (tax_id, fname, lname, address, phone_number, email, `password`, property_type, user_privileges) VALUES('096365432', 'Dimitris', 'Mal', 'Sokaki5', '6574875758','dimitris4@gmail.com', '123457', 'APARTMENT_BUILDING', 'USER');
+INSERT INTO OWNER (tax_id, fname, lname, address, phone_number, email, `password`, property_type, user_privileges) VALUES('123455889', 'Konstantinos', 'Brellas', 'Sok2', '4758026048','kwstas13@gmail.com', '12348756', 'VILLA', 'USER');
+INSERT INTO OWNER (tax_id, fname, lname, address, phone_number, email, `password`, property_type, user_privileges) VALUES('584739339', 'Dimitra', 'Tsol', 'Sokaki7', '2036906539','dimitra2@gmail.com', '12256458', 'VILLA', 'USER');
+INSERT INTO OWNER (tax_id, fname, lname, address, phone_number, email, `password`, property_type, user_privileges) VALUES('506937442', 'Dionisis', 'Theod', 'Sokaki8', '6384345601','nionios5@gmail.com', '4466378', 'COTTAGE', 'USER');
+INSERT INTO OWNER (tax_id, fname, lname, address, phone_number, email, `password`, property_type, user_privileges) VALUES('557753395', 'Xristos', 'Per', 'Sokaki0', '6943338265','xristos2@gmail.com', '976456', 'DETACHED_HOUSE', 'USER');
+INSERT INTO OWNER (tax_id, fname, lname, address, phone_number, email, `password`, property_type, user_privileges) VALUES('003765432', 'Dim', 'Maleas', 'Sokaki11', '6698536586','dimitris56@gmail.com', '2365757', 'APARTMENT_BUILDING', 'USER');
+INSERT INTO OWNER (tax_id, fname, lname, address, phone_number, email, `password`, property_type, user_privileges) VALUES('364826789', 'Kostas', 'Bls', 'Sok3', '6958693048','kwstas22@gmail.com', '3273572', 'VILLA', 'USER');
 
 
-INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2019-01-01', 'PENDING', 'PAINTING', 150.50, 'Karaiskaki', 1,'Lorem Ipsum');
-INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2019-02-02', 'IN_PROGRESS', 'INSULATION', 250.10, 'Toumpa', 2,'Lorem Ipsum');
-INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2019-03-03', 'FINISHED', 'PLUMBING_WORKS', 350.30, 'Enwsis', 3,'Lorem Ipsum');
-INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2019-04-04', 'FINISHED', 'ELECTRICAL_WORKS', 45.50, 'Athinoramas', 4,'Lorem Ipsum');
-INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2019-05-05', 'IN_PROGRESS', 'PAINTING', 55.70, 'Panepistimiou', 5,'Lorem Ipsum');
 
+INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2019-12-20', 'PENDING', 'PAINTING', 150.50, 'Karaiskaki', 5,'Lorem Ipsum');
+INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2019-12-08', 'IN_PROGRESS', 'INSULATION', 250.10, 'Toumpa', 5,'Lorem Ipsum');
+INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2019-03-03', 'FINISHED', 'PLUMBING_WORKS', 350.30, 'Enwsis', 6,'Lorem Ipsum');
+INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2019-04-04', 'FINISHED', 'ELECTRICAL_WORKS', 45.50, 'Athinoramas', 11,'Lorem Ipsum');
+INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2019-12-05', 'IN_PROGRESS', 'PAINTING', 55.70, 'Panepistimiou', 12,'Lorem Ipsum');
+INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2019-12-22', 'PENDING', 'PAINTING', 450.50, 'Karaiskaki3', 10,'Lorem Ipsum');
+INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2019-12-21', 'PENDING', 'INSULATION', 280.10, 'Toumpa2', 10,'Lorem Ipsum');
+INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2020-01-05', 'PENDING', 'PLUMBING_WORKS', 680.30, 'Enwsis5', 6,'Lorem Ipsum');
+INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2019-04-04', 'FINISHED', 'ELECTRICAL_WORKS', 450.50, 'Athinoramas7', 7,'Lorem Ipsum');
+INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2020-02-05', 'PENDING', 'PAINTING', 555.70, 'Panepistimiou8', 7,'Lorem Ipsum');
+INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2020-01-13', 'PENDING', 'PAINTING', 357.50, 'Karaiskaki56', 8,'Lorem Ipsum');
+INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2019-12-07', 'IN_PROGRESS', 'INSULATION', 345.10, 'Toumpa87', 8,'Lorem Ipsum');
+INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2019-07-08', 'FINISHED', 'PLUMBING_WORKS', 442.30, 'Enwsis08', 9,'Lorem Ipsum');
+INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2019-12-28', 'PENDING', 'ELECTRICAL_WORKS', 789.50, 'Athinoramas54', 9,'Lorem Ipsum');
+INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2019-12-04', 'IN_PROGRESS', 'PAINTING', 75.80, 'Panepistimiou76', 5,'Lorem Ipsum');
+INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2019-12-27', 'PENDING', 'PAINTING', 445.50, 'Karaiskaki34', 11,'Lorem Ipsum');
+INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2020-01-22', 'PENDING', 'INSULATION', 766.10, 'Toumpa54', 12,'Lorem Ipsum');
+INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2020-01-25', 'PENDING', 'PLUMBING_WORKS', 543.30, 'Enwsis76', 7,'Lorem Ipsum');
+INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2020-01-07', 'PENDING', 'ELECTRICAL_WORKS', 405.50, 'Athinoramas89', 8,'Lorem Ipsum');
+INSERT INTO REPAIR (`date`, repair_status, repair_type, cost, address, owner_id, `description`) VALUES('2019-12-10', 'IN_PROGRESS', 'PAINTING', 103.70, 'Panepistimiou34', 5,'Lorem Ipsum');
