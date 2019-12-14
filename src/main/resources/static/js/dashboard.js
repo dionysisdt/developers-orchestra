0f71ed894ffc7b5eb163cdfdd23fb366c5f8935d
Dionysios Theodoropoulos <dionysios.theodoropoulos@gmail.com>
2019-12-14 19:47:44 +0200

    Merged "templates" into "master"

---
+++ src/main/resources/static/js/dashboard.js
@@ -0,0 +1,58 @@
+/* globals Chart:false, feather:false */
+
+(function () {
+    'use strict'
+
+    feather.replace()
+
+    // Graphs
+    var ctx = document.getElementById('myChart')
+    // eslint-disable-next-line no-unused-vars
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [
+                'January',
+                'February',
+                'March',
+                'April',
+                'May',
+                'June',
+                'July',
+                'August',
+                'September',
+                'October',
+                'November',
+                'December'
+            ],
+            datasets: [{
+                data: [
+                    1339,
+                    2345,
+                    1483,
+                    2003,
+                    2489,
+                    2092,
+                    1034
+                ],
+                lineTension: 0,
+                backgroundColor: 'transparent',
+                borderColor: '#007bff',
+                borderWidth: 4,
+                pointBackgroundColor: '#007bff'
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: false
+                    }
+                }]
+            },
+            legend: {
+                display: false
+            }
+        }
+    })
+}())
