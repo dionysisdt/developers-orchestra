73a3ff041473e62a822627677a0805e56065f41d
kostasbls <kostasmprellas@yahoo.gr>
2019-12-14 17:59:37 +0200

    Created CRUD for property owner

---
+++ src/main/java/com/projectfuture/team0/developersorchestra/forms/OwnerForm.java
@@ -0,0 +1,88 @@
+package com.projectfuture.team0.developersorchestra.forms;
+
+import com.projectfuture.team0.developersorchestra.enums.PropertyType;
+
+public class OwnerForm {
+
+    private Long ownerID;
+    private String taxID;
+    private String firstName;
+    private String lastName;
+    private String ownerAddress;
+    private String phoneNumber;
+    private String email;
+    private String password;
+    private PropertyType propertyType;
+
+    public Long getOwnerID() {
+        return ownerID;
+    }
+
+    public void setOwnerID(Long ownerID) {
+        this.ownerID = ownerID;
+    }
+
+    public String getTaxID() {
+        return taxID;
+    }
+
+    public void setTaxID(String taxID) {
+        this.taxID = taxID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getOwnerAddress() {
+        return ownerAddress;
+    }
+
+    public void setOwnerAddress(String ownerAddress) {
+        this.ownerAddress = ownerAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public PropertyType getPropertyType() {
+        return propertyType;
+    }
+
+    public void setPropertyType(PropertyType propertyType) {
+        this.propertyType = propertyType;
+    }
+}
