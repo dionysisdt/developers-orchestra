c3a59424f54edea6de8dc1cb079a264bf6e996aa
dimitrismaleas <dimitriosmaleas@hotmail.com>
2019-12-19 01:56:35 +0200

    added single entry page when localhost is loading

---
+++ src/main/java/com/projectfuture/team0/developersorchestra/controller/PageController.java
@@ -0,0 +1,14 @@
+package com.projectfuture.team0.developersorchestra.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/page")
+    private String page() {
+        return "pages/page";
+    }
+
+}
---
+++ src/main/resources/static/assets/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    var height = $('.footer').height();
+    $('.sect-t-footer').css('height', height);
+});
+
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+});
